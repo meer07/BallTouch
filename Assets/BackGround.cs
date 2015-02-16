5c95af1e244b5ef895c84f3c0182bb57d2850496
海下 直哉 <naoya-kaige@garage.co.jp>
2015-02-16 19:06:27 +0900

    first commit

---
+++ Assets/BackGround.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackGround : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1));
+		//Vector2 scale = max * 2;
+
+		transform.localScale = max;
+	}
+}
---
+++ Assets/BallFactory.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallFactory : MonoBehaviour 
+{
+	public POSITION position;
+	public GameObject plus, minus;
+
+	// Use this for initialization
+	void Start () 
+	{
+		SetPosition ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void ShotBall()
+	{
+		float random = Random.Range (1, 3);
+	}
+
+	void SetPosition()
+	{
+		switch (position) 
+		{
+			case POSITION.LEFT:
+				transform.position = Camera.main.ViewportToWorldPoint(new Vector2(0.8f,0.8f));
+				break;
+			case POSITION.RIGHT:
+				transform.position = Camera.main.ViewportToWorldPoint(new Vector2(0.2f,0.8f));
+				break;
+		}
+	}
+
+	public enum POSITION
+	{
+		LEFT,
+		RIGHT,
+	}
+}
